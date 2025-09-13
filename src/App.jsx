b8236b53237b24ab9f9899956f189b77756f1bbe
DineshKumarKori <dineshkumarkori978@gmail.com>
2025-09-13 17:10:47 +0530

    add initial layout and styling for App component and global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import About from './components/about/About'
+import Home from './components/home/Home'
+import Navbar from './components/Navbar/Navbar'
+import Project from './components/project/Project'
+import Contact from './components/contact/Contact'
 
 function App() {
 
   return (
    <>
-   
+   <Navbar/>
+   <Home/>
+   <About/>
+   <Project/>
+   <Contact/>
    </>
   )
 }
